c0b42468db1e1a3deab549e47a8922ef427dfc3f
Colton Neves <colton@simcase.io>
2019-06-14 13:30:08 -0400

    added rendertags method

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -3,31 +3,36 @@ import React, { Component } from 'react';
 class Counter extends Component {
     state = {
         count: 1,
-        tags:['tag1', 'tag2', 'tag3'],
+        tags:[]
     };
+
+    renderTags() {
+        if(this.state.tags.length === 0) return <p>There are no tags!</p>;
+        return this.state.tags.map(tag => <li key={tag}> {tag} </li>);
+    }
 
     render() {
 
         return (
             <div>
-                <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
-                <button className="btn btn-secondary btn-sm">Increment</button>
+                {/* <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+                <button className="btn btn-secondary btn-sm">Increment</button> */}
                 <ul>
                     {this.state.tags.map(tag => <li key={tag}>{tag}</li>)}
                 </ul>
             </div>);
     }
 
-    getBadgeClasses() {
-        let classes = "badge m-2 ";
-        classes += (this.state.count === 0) ? "badge-warning" : "badge-primary";
-        return classes;
-    }
+    // getBadgeClasses() {
+    //     let classes = "badge m-2 ";
+    //     classes += (this.state.count === 0) ? "badge-warning" : "badge-primary";
+    //     return classes;
+    // }
 
-    formatCount() {
-        const { count } = this.state;
-        return count === 0 ? 'Zero' : count;
-    }
+    // formatCount() {
+    //     const { count } = this.state;
+    //     return count === 0 ? 'Zero' : count;
+    // }
 }
  
 export default Counter;
