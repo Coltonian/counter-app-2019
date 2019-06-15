6c42868334f50509ed6534b0bf5db127f901ffc5
Colton Neves <colton@simcase.io>
2019-06-15 15:04:19 -0400

    added increment method

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -6,33 +6,32 @@ class Counter extends Component {
         tags:[]
     };
 
-    renderTags() {
-        if(this.state.tags.length === 0) return <p>There are no tags!</p>;
-        return this.state.tags.map(tag => <li key={tag}> {tag} </li>);
+    handleIncrement = () => {
+        this.setState({ count: this.state.count + 1 });
     }
 
     render() {
 
         return (
             <div>
-                {/* <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
-                <button className="btn btn-secondary btn-sm">Increment</button> */}
+                <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+                <button onClick={this.handleIncrement}className="btn btn-secondary btn-sm">Increment</button>
                 <ul>
                     {this.state.tags.map(tag => <li key={tag}>{tag}</li>)}
                 </ul>
             </div>);
     }
 
-    // getBadgeClasses() {
-    //     let classes = "badge m-2 ";
-    //     classes += (this.state.count === 0) ? "badge-warning" : "badge-primary";
-    //     return classes;
-    // }
+    getBadgeClasses() {
+        let classes = "badge m-2 ";
+        classes += (this.state.count === 0) ? "badge-warning" : "badge-primary";
+        return classes;
+    }
 
-    // formatCount() {
-    //     const { count } = this.state;
-    //     return count === 0 ? 'Zero' : count;
-    // }
+    formatCount() {
+        const { count } = this.state;
+        return count === 0 ? 'Zero' : count;
+    }
 }
  
 export default Counter;
